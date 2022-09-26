b0358d3d8927536dcd4fa768f16f2d3c0feae8fe
Melanie Roberts <robertsmelaniecc@gmail.com>
2022-09-26 19:35:03 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,46 @@
+// JavaScript Basics Exercise
+// 1
+console.log(77);
+
+//   2
+/*
+Numbers
+Strings
+Null
+Undefined
+*/
+
+//  3
+// + - * / % **
+
+//  4
+console.log(5 % 4);
+console.log(5 % 2);
+
+//  5
+console.log('I"M MARY POPPINS, Y"ALL!');
+
+//  6
+console.log("Hawk" + "eye");
+
+//  7
+console.log("Melanie Roberts".length);
+
+//  8
+console.log('Avengers: Endgames'[2]);
+
+//  Bonus 1
+console.log('\\');
+
+//  Bonus 2
+console.log(5 ** 4 - 9);
+
+//  Bonus 3
+console.log('I' + ' am' +' Iron' +'Man' + ' ' + ''.length);
+console.log('I'.length - 'am'.length + 'Iron'.length - 'Man'.length);
+console.log('I'.length - 'am'.length + 'Iron'.length % 'Man'.length);
+// Bonus 4
+console.log('CAP'[1]+ 'VISION'[0]+ 'OKOYE'[4]+ 'NICK FURY'[0]+ 'GROOT'[0]+ 'DOCTOR STRANGE'[13]+ 'THOR'[3]+ 'STAR-LORD'[0]);
+
+//  BONUS 5
+console.log(m=25, c=4, u=m+c, 'm+c=',u);
