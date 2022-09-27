84892beaca50a3d405d25ba502f66c7164843806
Melanie Roberts <robertsmelaniecc@gmail.com>
2022-09-27 10:52:47 -0400

    corrections

--- script.js
+++ script.js
@@ -6,31 +6,46 @@ console.log(77);
 /*
 Numbers
 Strings
+Boolean
 Null
 Undefined
 */
 
 //  3
-// + - * / % **
+/* 
+Addition + 
+Subtraction - 
+Multiplication * 
+Division / 
+Modulus % 
+Exponentiation **
 
+Extra:
+increment ++
+decrement --
+
+*/
 //  4
 console.log(5 % 4);
 console.log(5 % 2);
 
 //  5
-console.log('I"M MARY POPPINS, Y"ALL!');
+// incorrect console.log('I"M MARY POPPINS, Y"ALL!');
+console.log('I\'M MARY POPPINS, Y\'ALL');
 
 //  6
 console.log("Hawk" + "eye");
 
 //  7
 console.log("Melanie Roberts".length);
+console.log("Melanie".length + "Roberts".length);
 
 //  8
-console.log('Avengers: Endgames'[2]);
+console.log('Avengers: Endgames'[16]);
 
 //  Bonus 1
 console.log('\\');
+console.log('\/');
 
 //  Bonus 2
 console.log(5 ** 4 - 9);
@@ -39,8 +54,32 @@ console.log(5 ** 4 - 9);
 console.log('I' + ' am' +' Iron' +'Man' + ' ' + ''.length);
 console.log('I'.length - 'am'.length + 'Iron'.length - 'Man'.length);
 console.log('I'.length - 'am'.length + 'Iron'.length % 'Man'.length);
+console.log(('I'.length + 'am'.length + 'Iron'.length + 'Man'.length) % 10);
+console.log(('I'.length + 'am'.length + 'Iron'.length + 'Man'.length) * 0);
+
+
 // Bonus 4
-console.log('CAP'[1]+ 'VISION'[0]+ 'OKOYE'[4]+ 'NICK FURY'[0]+ 'GROOT'[0]+ 'DOCTOR STRANGE'[13]+ 'THOR'[3]+ 'STAR-LORD'[0]);
+//console.log('CAP'[1]+ 'VISION'[0]+ 'OKOYE'[4]+ 'NICK FURY'[0]+ 'GROOT'[0]+ 'DOCTOR STRANGE'[13]+ 'THOR'[3]+ 'STAR-LORD'[0]);
+console.log('CAP'[1]);
+console.log('VISION'[0]);
+console.log('OKOYE'[4]);
+console.log('NICK FURY'[0]);
+console.log('GROOT'[0]);
+console.log('DOCTOR STRANGE'[13]);
+console.log('THOR'[3]);
+console.log('STAR-LORD'[0]);
 
 //  BONUS 5
-console.log(m=25, c=4, u=m+c, 'm+c=',u);
+//incorrect console.log(m=25, c=4, u=m+c, 'm+c=',u);
+var m=25;
+var c=4;
+var u = m+c;
+console.log(u);
+
+//With let
+/*
+let m = 25;
+let c = 4;
+let u = m + c
+console.log(u);
+*/
